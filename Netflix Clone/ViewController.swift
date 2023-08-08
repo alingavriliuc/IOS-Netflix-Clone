b0ca4b52b2a9055e87de15ac5a82dce5d8ca6e8f
Alin Gavriliuc <alingavriliuc@hotmail.com>
2023-08-08 23:05:17 +0200

    Initial Commit

---
+++ Netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Clone
+//
+//  Created by Alin Gavriliuc on 08/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
